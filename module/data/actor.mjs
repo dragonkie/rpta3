7572854fa1e337135a2150cce06029fd0c7af9a1
Ásta <43883937+dragonkie@users.noreply.github.com>
2026-06-13 17:01:30 -0400

    Update actor.mjs
    fixed migration ordering issue

--- module/data/actor.mjs
+++ module/data/actor.mjs
@@ -95,14 +95,13 @@ export default class ActorData extends DataModel {
   }
 
   static migrateData(source) {
-    super.migrateData(source);
     console.log('migrating data', source.hp)
     //migrates previous max hp value to the new base value so that the max one can be used 
     //to show total hp on resource bars with tokens
     source.hp.base = source.hp.max;
     delete source.hp.max;
 
-    return source;
+    return super.migrateData(source);
   }
 
   prepareBaseData() {
